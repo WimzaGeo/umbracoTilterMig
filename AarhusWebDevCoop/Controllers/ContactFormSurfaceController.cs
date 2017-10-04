4e79a34ca78c78b1c32636c4223c68cdf62e42f6
Unknown <christianporsfelt@hotmail.com>
2017-10-04 14:48:19 +0200

    Grisen G

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,39 @@
+﻿using AarhusWebDevCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            MailMessage message = new MailMessage();
+            message.To.Add("umbracomailermand@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message + "\n my email is: " + model.Email;
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network; smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "stmp.gmail.com"; smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("umbracomailermand@gmail.com", "jegTilter123");
+                smtp.EnableSsl = true;
+                smtp.Send(message);
+            }
+                return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
